8cdfbd3153914594ff2b016391db1313cd718384
juliazrp <julizavrr@gmail.com>
2018-02-12 20:55:05 +0300

    добавлена сортировка подсчетом

--- pr1/pr1/Source.cpp
+++ pr1/pr1/Source.cpp
@@ -9,7 +9,6 @@ int main()
 	int mas[20001];
 	int i, j, k, l = 0, min;
 	for (i = 0; i<20000; i++)
-		mas[i] = 0 + rand() % 10;
 	for (j = 0; j < 20000; j++)
 	{
 		k = i; min = mas[i];
@@ -24,6 +23,24 @@ int main()
 		mas[k] = mas[j];
 		mas[j] = min;
 		//cout << mas[j]<<endl;
+	}
+	//count
+	int c[10];
+	int mas1[200001];
+	for (i = 0; i<200000; i++)
+		mas1[i] = 0 + rand() % 10;
+	for (i = 0; i < 10; i++)
+		c[i] = 0;
+	for (i = 0; i < 200000; i++) {
+		c[mas1[i]] = c[mas1[i]] + 1;
+	}
+	i = 0;
+	for (j = 0; j < 10; j++) {
+		while (c[j] != 0) {
+			mas1[i] = j;
+			c[j]--;
+			i++;
+		}
 	}
 	system("pause");
 	return 0;
